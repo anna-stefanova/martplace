596d2c1c6895dabf81fa03c6c6f73370ca6896df
anna-stefanova <astefanova267@gmail.com>
2021-10-20 15:14:23 +0300

    added newest products section and followers products slider

--- app/js/main.js
+++ app/js/main.js
@@ -4,12 +4,25 @@ $(function () {
         starWidth: "12px",
         ratedFill: "#ffa726"
     });
+    $(".star-rate").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        ratedFill: "#ffa726"
+    });
 
-    $('.owl-carousel').owlCarousel({
+    $('.weekly__inner').owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
         items: 1,
+        dots: false,
+        smartSpeed: 650,
+    });
+    $('.followers__inner').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        items: 3,
         dots: false,
         smartSpeed: 650,
     });
